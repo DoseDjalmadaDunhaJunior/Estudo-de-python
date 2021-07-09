fceb60af5708c0a15c124824c6d7847608af1ae0
DoseDjalmadaDunhaJunior <djalminhajr_cunha@hotmail.com>
2021-07-09 15:48:04 -0300

    colocado collections

--- Arquivos/Deque.py
+++ Arquivos/Deque.py
@@ -1,1 +1,22 @@
-print("oi")
+"""
+Deque
+é uma lista de alta performace
+
+"""
+
+from collections import deque
+
+#criando deques
+
+deq = deque("josé djama")
+print(deq)
+
+# adicionando elementos no deque
+deq.append('@')
+print(deq)
+deq.remove('@')
+print(deq)
+deq.pop()
+print(deq)
+deq.append('a')
+print(deq)
